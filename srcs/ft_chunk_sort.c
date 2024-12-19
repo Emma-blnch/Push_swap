44084e50a2c440c8a865fe1ade140b3334d79aaf
Emma Blanchard <emma.blnch4@gmail.com>
2024-12-19 17:00:33 +0100

    debug messages added

--- srcs/ft_chunk_sort.c
+++ srcs/ft_chunk_sort.c
@@ -39,14 +39,25 @@ void	move_chunk_to_b(t_stack *stack_a, t_stack *stack_b, int min, int max)
 
 	i = 0;
     size = stack_a->size;
+	 printf("Moving chunk to B: min = %d, max = %d\n", min, max);
 	while (i < size)
 	{
 		if (stack_a->top->value >= min && stack_a->top->value < max)
+		{
+			 printf("Pushing %d from A to B\n", stack_a->top->value);
 			pb(stack_b, stack_a);
+		}
 		else
+		{
+			printf("Rotating A: %d\n", stack_a->top->value);
 			ra(stack_a);
+		}
 		i++;
 	}
+	printf("Chunk moved to B. Stack A:\n");
+    print_stack(stack_a);
+    printf("Stack B:\n");
+    print_stack(stack_b);
 }
 
 int	find_insert_position(t_stack *stack_a, int value)
@@ -72,10 +83,16 @@ void	move_chunk_back_to_a(t_stack *stack_a, t_stack *stack_b)
 
 	while (stack_b->size > 0)
 	{
+		printf("Stack B before moving back to A:\n");
+        	print_stack(stack_b);
 		pos = find_insert_position(stack_a, stack_b->top->value);
+		printf("Insert position in A for %d is %d\n", stack_b->top->value, pos);
 		smart_rotate(stack_a, pos);
 		pa(stack_a, stack_b);
+		printf("Moved %d from B to A\n", stack_b->top->value);
 	}
+	printf("All chunks moved back to A. Stack A:\n");
+   	print_stack(stack_a);
 }
 
 void	smart_rotate(t_stack *stack, int pos)
@@ -109,6 +126,7 @@ void    chunk_sort(t_stack *stack_a, t_stack *stack_b)
         chunk_count = stack_a->size / 10;
     chunk_size = stack_a->size / chunk_count;
     i = 0;
+	 printf("Starting chunk sort with %d chunks\n", chunk_count);
     while (i < chunk_count)
     {
         min = i * chunk_size;
@@ -116,8 +134,10 @@ void    chunk_sort(t_stack *stack_a, t_stack *stack_b)
             max = INT_MAX;
         else
             max = (i + 1) * chunk_size;
+	printf("Processing chunk %d: min = %d, max = %d\n", i, min, max);
         move_chunk_to_b(stack_a, stack_b, min, max);
         i++;
     }
     move_chunk_back_to_a(stack_a, stack_b);
+	printf("Chunk sort completed. Final stack A:\n");
 }
